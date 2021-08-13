3eb0390537467356b00956ece0c440e5335ded4c
Nitish Kafle <kaflenit@gmail.com>
2021-08-13 11:31:48 -0400

    defined data object and query

--- app/src/main/java/com/codepath/nitishkafle/parstagram/ParseApplication.java
+++ app/src/main/java/com/codepath/nitishkafle/parstagram/ParseApplication.java
@@ -3,12 +3,16 @@ package com.codepath.nitishkafle.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("TsHIN8JN6uCI9C3xZJu6cCWMeAZ1cJg0ed9ONRop")
